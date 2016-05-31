5c3fe06f65db3b48286793db1dbad820aba79fbf
Han-Byul Kim <shine_star_11@Han-Byuls-MacBook-Pro.local>
2016-05-31 22:31:04 +0900

    design revised

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -19,7 +19,7 @@ class TaskController < ApplicationController
     @tasksend = task1.where(:team_id => params[:team]).order(duedate: :asc)
     task2 = current_user.tasks
     @taskdo = task2.where(:team_id => params[:team])
-    @task100 = @taskdo.where(:wansungdo => 100)
+    @task100 = task2.where(:wansungdo => 100).order(duedate: :asc)
     @tasklist = @taskdo.order(duedate: :asc)
     @tasklistdesc = @taskdo.order(duedate: :desc)
     @team = Team.find(params[:team])
