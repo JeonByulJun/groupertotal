e2e73a45b3df845d75cd590b2ed305d73b462d41
EC2 Default User <ec2-user@ip-172-31-16-179.ap-northeast-2.compute.internal>
2016-05-31 08:33:56 +0000

    uploader set

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,2 +1,11 @@
 # Allow non-ascii letters in uploaded filenames
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\s\.\-\+]/
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     'AKIAIWPG5TOBBQX7N3OQ',                        # required
+    aws_secret_access_key: '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',                        # required
+    region:                'ap-northeast-2',                  # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = 'grouperteam'                          # required
+end
